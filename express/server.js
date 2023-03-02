36392ba50420234f4a1bb65327df582be060a07e
Taras Pustovyi <taraselnik@gmail.com>
2023-03-03 00:05:56 +0200

    new

--- express/server.js
+++ express/server.js
@@ -1,47 +1,33 @@
-'use strict';
-import * as dotenv from 'dotenv';
-dotenv.config({path: '.env'});
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+// import { errorHandler } from './middlewares/error.js';
 
-import express, { Router } from 'express';
-// import { join } from 'path';
-import serverless from 'serverless-http';
-// import mongoose from 'mongoose';
+dotenv.config({path: '../.env'});
 
 const app = express();
-
-// const URI = `${process.env.DATABASE_URL}`;
-
-// mongoose.set("strictQuery", false);
-// mongoose.connect(URI);
-
-// const db = mongoose.connection;
-// db.on('error', (error) => console.log(error))
-// db.once('open', () => console.log('DataBase connected...'))
-
-
-const router = Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-
-router.get('/api',async (req, res) => {
-  res.send('Hello World!')
-});
-
-
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-// app.use('/', (req, res) => res.sendFile(join(__dirname, '../index.html')));
 
-export default app;
-export const handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  const handler = serverless(app);
-  const result = handler(event, context);
-  return result;
-};
+// set up your routes here
+
+app.get('/.netlify/functions/api', (req, res) => {
+  res.send('Welcome')
+});
+
+mongoose
+  .connect(`${process.env.MONGO_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+
--- server-local.js
+++ server-local.js
@@ -1,5 +1,5 @@
 'use strict';
 
-import app from './express/server.js';
+import app from './express/DDDserver';
 
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
