5a134c178c3066e16d1f34818889259e2c59c18d
Taras Pustovyi <taraselnik@gmail.com>
2023-03-02 22:25:52 +0200

    ES

--- express/server.js
+++ express/server.js
@@ -20,28 +20,27 @@ db.once('open', () => console.log('DataBase connected...'))
 
 
 const router = Router();
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
 
-router.get('/api', async (req, res) => {
+router.get('/api', (req, res) => {
   res.send('Hello World!')
 });
 
 
-router.get('/another', async (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', async (req, res) => res.json({ postBody: req.body }));
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(express.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 // app.use('/', (req, res) => res.sendFile(join(__dirname, '../index.html')));
 
 export default app;
-
-const handler = serverless(app);
-module.exports.handler = async (event, context) => {
+export const handler = async (event, context) => {
+  const handler = serverless(app);
   const result = await handler(event, context);
   return result;
 };
